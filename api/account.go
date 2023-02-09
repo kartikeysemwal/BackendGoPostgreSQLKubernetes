525455584e3cb4e624e9824c4fc1ec19297137d3
Kartikey Semwal <55513264+kartikeysemwal@users.noreply.github.com>
2023-02-09 22:16:48 +0530

    testing branch

--- api/account.go
+++ api/account.go
@@ -51,7 +51,9 @@ type getAccountRequest struct {
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
+
 	var req getAccountRequest
+
 	if err := ctx.ShouldBindUri(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
